83e9ddbb70e18101f16f37afae19eb5741ab75be
Penguinpeak jrdev <penguinpeakjrdev@gmail.com>
2026-08-04 17:15:05 +0530

    add GTM script

--- app/layout.tsx
+++ app/layout.tsx
@@ -89,13 +89,36 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           .goog-text-highlight { background-color: transparent !important; border: none !important; box-shadow: none !important; }
         `}</style>
   
-        
+        <Script id="gtm" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){
+              w[l]=w[l]||[];
+              w[l].push({'gtm.start': new Date().getTime(), event:'gtm.js'});
+              var f=d.getElementsByTagName(s)[0],
+                  j=d.createElement(s),
+                  dl=l!='dataLayer'?'&l='+l:'';
+              j.async=true;
+              j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+              f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-TB95FJBR');
+          `}
+        </Script>
 
       </head>
 
 
       <body style={{ minHeight: "100vh", display: "flex", flexDirection: "column" }} suppressHydrationWarning>
         
+        {/* <!-- Google Tag Manager (noscript) --> */}
+       <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-TB95FJBR"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+              
         {/* Google Analytics (GA4) */}
           <Script
             src="https://www.googletagmanager.com/gtag/js?id=G-GMVBEMSTPL"
